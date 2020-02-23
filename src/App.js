b877555503e5ee5df4ed281ec58009f4a18e1713
xinxia <xinxia@tujia.com>
2020-02-23 23:47:18 +0800

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GlobalStyled } from './style.js';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyled/>
+      hello
+    </div>
+  );
+}
+
+export default App;
